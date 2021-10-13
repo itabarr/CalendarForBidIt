14147033110d51f82f668448bb8af4bd42134b32
itabarr <61348186+itabarr@users.noreply.github.com>
2021-10-13 15:26:22 +0300

    Added tests information

--- popup.js
+++ popup.js
@@ -1,6 +1,3 @@
-// Snag our button
-
-
 let btn = document.getElementById("download")
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -17,10 +14,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         btn.disabled = true;
     }
     });
-
-
-
-
 
 function pass_localstorage(){
     let biditDisplaySettingsCookie = localStorage.biditDisplaySettingsCookie;
@@ -57,6 +50,48 @@ function parse_bidit_storage(biditDisplaySettingsCookie){
         let course = data_[course_key]
         let groups = course['groups'];
 
+        let num_of_tests = course.moeds.length;
+        let arr_ = Array.from({length: num_of_tests}, (v, i) => i);
+
+
+
+
+
+
+
+        for (const i in arr_){
+            start_str = (course.startMoedHours[i]);
+            start_int = parseInt(start_str.substring(0,2));
+            let end_int = start_int + 3;
+            d = new Date();
+            d.setHours(end_int)
+            end_hours_str = `${d.getHours().toString()}:00`
+
+            dateString = course.dates[i];
+            date_str = dateString.substr(3, 2)+"/"+dateString.substr(0, 2)+"/"+dateString.substr(6, 4);
+
+
+            test_event ={
+                Name: course.cName,
+                Moed : course.moeds[i],
+                Type : course.types[i],
+                StartHour : course.startMoedHours[i],
+                EndHour: end_hours_str,
+                Day : date_str
+            }
+
+            let formatted_start =  `${test_event.Day} ${test_event.StartHour}`
+            let formatted_end = `${test_event.Day} ${test_event.EndHour}`
+
+            test_event.formatted_start = formatted_start;
+            test_event.formatted_end = formatted_end;
+
+            console.log(test_event);
+            cal.addEvent(  'מועד ' + test_event.Moed +' '+ test_event.Name, test_event.Moed  , "", formatted_start ,formatted_end);
+
+
+        }
+
         for (const group_key in groups){
             let group = groups[group_key]
             if (group['gChosen'] == true){
@@ -65,7 +100,7 @@ function parse_bidit_storage(biditDisplaySettingsCookie){
                 let arr = Array.from({length: num_of_days}, (v, i) => i);
                 for (const i in arr){
 
-                    let course_event = {
+                    course_event = {
                         Name: course.cName,
                         Type: group.ofenHoraa,
                         Day: format_days(group.daysArr[i]),
@@ -76,9 +111,12 @@ function parse_bidit_storage(biditDisplaySettingsCookie){
 
                     let formatted_start = `09/10/2021 ${course_event.StartHour}`
                     let formatted_end = `09/10/2021  ${course_event.EndHour}`
+
+
+
                     //{freq: 'WEEKLY', interval: 1, byday: [course_event.Day] }
 
-                    cal.addEvent(course_event.Name, course_event.Type, course_event.Location, formatted_start ,formatted_end,{freq: 'WEEKLY', interval: 1, byday: [course_event.Day] , until:'09/01/2022'});
+                    cal.addEvent(course_event.Name, course_event.Type, course_event.Location, formatted_start ,formatted_end,{freq: 'WEEKLY', interval: 1, byday: [course_event.Day] , until:'01/09/2022'});
                 };
             };
 
@@ -92,14 +130,6 @@ function parse_bidit_storage(biditDisplaySettingsCookie){
 btn.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({active: true, currentWindow:true}) // Find current tab
 
-    let domain = new URL(tab.url);
-    if (domain.hostname == 'bid-it.appspot.com'){
-        alert("You're in the BidIt website!")
-    }
-    else{
-        alert("You're NOT in the BidIt website!")
-        return
-    }
     const script_ = await chrome.scripting.executeScript({ // Run the following script on our tab
         target: {tabId: tab.id},
         func: pass_localstorage
